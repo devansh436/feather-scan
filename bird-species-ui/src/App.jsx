9c8ce986f820c788da180fe7c0aeb255cfab9ffd
Devansh Deshpande <devansh436@gmail.com>
2025-03-14 13:46:36 +0530

    Added file upload page component

--- bird-species-ui/src/App.jsx
+++ bird-species-ui/src/App.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React  from "react";
+import UploadPage from "./pages/uploadPage.jsx"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/upload" element={<UploadPage />}></Route>
+      </Routes>
+    </Router>
+  )
+}
+
+function Home() {
   return (
     <div className="container-fluid bg-success text-white min-vh-100 d-flex flex-column align-items-center justify-content-center">
       <h1 className="display-3 fw-bold">🦜 Feather Scan</h1>
       <p className="lead text-light text-center">
         Upload an image and let AI identify the bird species instantly!
       </p>
-      <button className="btn btn-light text-success fw-bold mt-3">Get Started</button>
+      <Link to="/upload" className="btn btn-light text-success fw-bold mt-3">
+        Get Started
+      </Link>
     </div>
   );
 }
