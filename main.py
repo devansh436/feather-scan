153eb33e16825a972751effd4f7b9fc4e370905d
Devansh Deshpande <devansh436@gmail.com>
2026-01-04 23:17:16 +0530

    Remove __pycache__ from repo

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # https://bird-species-model.onrender.com
 # uvicorn main:app --host 0.0.0.0 --port 5000
+
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 from PIL import Image
 import torch
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import io
 
@@ -22,25 +23,31 @@ model = AutoModelForImageClassification.from_pretrained("chriamue/bird-species-c
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 model.eval()
+torch.set_grad_enabled(False)
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     try:
         image_bytes = await file.read()
+        if len(image_bytes) > 5 * 1024 * 1024:
+            raise HTTPException(status_code=413, detail="File too large.")
+        
+        # raw bytes -> mem buffer -> PIL Image -> RGB
+        # convert("RGB") because model expects 3 channel input
         image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
         inputs = processor(images=image, return_tensors="pt").to(device)
 
-        with torch.no_grad():
+        with torch.inference_mode():
             outputs = model(**inputs)
-            logits = outputs.logits
-            probs = torch.nn.functional.softmax(logits, dim=-1)
+            logits = outputs.logits # logits is raw weighted sum, not softmaxxed yet
+            probs = torch.nn.functional.softmax(logits, dim=-1) # softmax fn
             pred_idx = torch.argmax(probs, dim=-1)
             confidence = round(100*probs[0, pred_idx].item(), 2)
             label = model.config.id2label[pred_idx.item()].title()
 
         return {"label": label, "confidence": confidence}
     except Exception as e:
-        return {"error": str(e)}
+        return HTTPException(status_code=500, detail=str(e))
 
 @app.get("/test")
 async def health():
