6d76c671b6d088637ce9a4ea0ec89267a9543252
Devansh Deshpande <devansh436@gmail.com>
2026-01-19 19:50:19 +0530

    fix server/config/firebase.ts for render

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -4,14 +4,24 @@
 */
 
 import admin from 'firebase-admin';
-
+import fs from 'fs';
 let initialized = false;
 
 export const initFirebase = async () => {
   if (initialized || process.env.NODE_ENV === 'test') return;
 
-  const serviceAccountKey = require('../../serviceAccountKey.json');
-  // Initialise firebase app
+  const serviceAccountPath =
+    process.env.RENDER === "true"
+      ? "/etc/secrets/serviceAccountKey.json"
+      : "serviceAccountKey.json"; // local fallback  // Initialise firebase app
+
+  let serviceAccountKey;
+  if (admin.apps.length === 0) {
+    serviceAccountKey = JSON.parse(
+      fs.readFileSync(serviceAccountPath, "utf8")
+    );
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert(
       serviceAccountKey as admin.ServiceAccount
